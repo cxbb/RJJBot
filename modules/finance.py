d05ffd921e2a0512528b8040d03ce46680735dc6
cxbb <cxxliu@gmail.com>
2015-07-10 02:43:50 +0000

    Finance module added NASDAQ

--- modules/finance.py
+++ modules/finance.py
@@ -5,7 +5,8 @@ import random
 
 class FinanceModule(BaseModule):
 
-  HSI_URL = 'http://finance.yahoo.com/webservice/v1/symbols/%5EHSI/quote?format=json&view=detail'
+  QUOTE_URL = { 'hsi': 'http://finance.yahoo.com/webservice/v1/symbols/%5EHSI/quote?format=json&view=detail',
+                'nasdaq': 'http://finance.yahoo.com/webservice/v1/symbols/%5EIXIC/quote?format=json&view=detail', }
 
   def __init__(self):
     pass
@@ -16,14 +17,15 @@ class FinanceModule(BaseModule):
     stripped = m['text'].strip()
     sender_id = m['from']['id']
     sender = m['from']['first_name']
-    if stripped == '/hsi':
+    if stripped == '/hsi' or stripped == '/nasdaq':
       try:
-        p = subprocess.Popen(['curl', '-L', '-k', FinanceModule.HSI_URL], stdout=subprocess.PIPE)
+        p = subprocess.Popen(['curl', '-L', '-k', FinanceModule.QUOTE_URL[stripped[1:]]], stdout=subprocess.PIPE)
         out, err = p.communicate()
         obj = json.loads(out)
-        hsi_price = obj['list']['resources'][0]['resource']['fields']['price']
-        hsi_date = obj['list']['resources'][0]['resource']['fields']['utctime']
-        return 'Hang Seng Index at %s: %s' % (hsi_date, hsi_price)
+        price = obj['list']['resources'][0]['resource']['fields']['price']
+        name = obj['list']['resources'][0]['resource']['fields']['name']
+        date = obj['list']['resources'][0]['resource']['fields']['utctime']
+        return '%s at %s: %s' % (name, date, price)
       except Exception, e:
         return 'Enjoy. Error: %s' % (str(e))
 
