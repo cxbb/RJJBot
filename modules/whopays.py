e500b463e0f260f29e07fd1637be646c34342df3
cxbb <cxxliu@gmail.com>
2016-01-22 03:21:11 +0000

    Lunch module add features

--- modules/whopays.py
+++ modules/whopays.py
@@ -13,6 +13,16 @@ class WhoPaysModule(BaseModule):
     self.sampled = []
     self.votes = {}
 
+  @staticmethod
+  def max_index(li):
+    max_v = li[0]
+    max_i = 0
+    for i in xrange(1, len(li)):
+      if li[i] > max_v:
+        max_v = li[i]
+        max_i = i
+    return max_i
+
   def process_message(self, m):
     if m.get('text') is None:
       return None
@@ -24,9 +34,41 @@ class WhoPaysModule(BaseModule):
         p = subprocess.Popen(['curl', '-L', '-k', WhoPaysModule.SCRIPT_URL], stdout=subprocess.PIPE)
         out, err = p.communicate()
         obj = json.loads(out)
-        who = obj['whoPays']
-        amount = obj['amount']
-        return 'Today {0} will pay for lunch. (Amount owed: ${1:,.1f})'.format(who, amount)
+        idx = WhoPaysModule.max_index(obj)
+        x1 = obj[idx]
+        obj = obj[:idx-1]+obj[idx+1:]
+        who = x1['name']
+        amount = x1['amount']
+
+        idx = WhoPaysModule.max_index(obj)
+        x1 = obj[idx]
+        obj = obj[:idx-1]+obj[idx+1:]
+        who2 = x1['name']
+        amount2 = x1['amount']
+
+        idx = WhoPaysModule.max_index(obj)
+        x1 = obj[idx]
+        obj = obj[:idx-1]+obj[idx+1:]
+        who3 = x1['name']
+        amount3 = x1['amount']
+
+        idx = WhoPaysModule.max_index(obj)
+        x1 = obj[idx]
+        obj = obj[:idx-1]+obj[idx+1:]
+        who4 = x1['name']
+        amount4 = x1['amount']
+
+        idx = WhoPaysModule.max_index(obj)
+        x1 = obj[idx]
+        obj = obj[:idx-1]+obj[idx+1:]
+        who5 = x1['name']
+        amount5 = x1['amount']
+
+        return 'Today {0} will pay for lunch. (Amount owed: ${1:,.1f})'.format(who, amount) + '\n' + \
+          'Backup 1: {0} (Amount owed: ${1:,.1f})'.format(who2, amount2) + '\n' + \
+          'Backup 2: {0} (Amount owed: ${1:,.1f})'.format(who3, amount3) + '\n' + \
+          'Backup 3: {0} (Amount owed: ${1:,.1f})'.format(who4, amount4) + '\n' + \
+          'Backup 4: {0} (Amount owed: ${1:,.1f})'.format(who5, amount5)
       except Exception, e:
         return 'Enjoy. Error: %s' % (str(e))
 
@@ -59,6 +101,7 @@ class WhoPaysModule(BaseModule):
 
     if stripped.startswith('/eataddsample '):
        try:
+         #return { 'type': 'sticker', 'content': 'BQADBQADVAMAAjlAVwEBBZ9yV_AV5AI' }
          restaurant = ' '.join(stripped.split(' ')[1:]).strip()
          if not self.add_sample ( restaurant ):
            raise Exception(e)
@@ -73,7 +116,7 @@ class WhoPaysModule(BaseModule):
          if self.vote(sender_id, sender, index):
            return '%s voted for %s.' % (sender, self.sampled[index])
          else:
-           raise Exception(e)
+           raise Exception('')
        except Exception, e:
          print str(e)
          return '%s: Invalid vote. Use /eatvote [1-%s]' % (sender, len(self.sampled))
